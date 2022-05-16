471a9c22fa40413a9ae2d0c976fb6dbc8307bb9d
Tobias Briones <tobiasbriones.dev@gmail.com>
2022-05-15 18:24:26 -0600

    Implement logic for status START

--- server/client.go
+++ server/client.go
@@ -43,7 +43,49 @@ func (c *Client) run() {
 }
 
 func (c *Client) listenMessage() {
+	log.Println("Listening for client message")
+	msg, err := readMessage(c.conn)
+	requireNoError(err)
+	c.onMessage(msg)
+}
 
+func (c *Client) onMessage(msg Message) {
+	log.Println("Message received:", msg)
+	switch msg.Status {
+	case Start:
+		c.start(msg)
+	default:
+		c.error("Wrong message status")
+	}
+}
+
+func (c *Client) start(msg Message) {
+	payload, err := msg.StartPayload()
+	requireNoError(err)
+	c.req = payload
+	c.status = Data
+	c.count = 0
+
+	switch payload.Action {
+	case ActionUpload:
+		c.startUpload(payload)
+	case ActionDownload:
+		c.startDownload(payload)
+	}
+}
+
+func (c *Client) startUpload(payload StartPayload) {
+	if payload.Size <= 0 {
+		c.error("File sent is empty")
+		return
+	}
+	CreateFile(payload.RelPath)
+	log.Println("Payload saved, writing status=DATA", payload)
+	writeStatus(Data, c.conn)
+}
+
+func (c *Client) startDownload(msg StartPayload) {
+	// TODO
 }
 
 func (c *Client) listenData() {
@@ -60,5 +102,6 @@ func (c *Client) overflows(chunk []byte) bool {
 
 func (c *Client) error(msg string) {
 	// TODO update func to accept msg
+	c.status = Error
 	writeStatus(Error, c.conn)
 }
