4ea0eab0e96306fb24e6231458523c74f6210383
Tobias Briones <tobiasbriones.dev@gmail.com>
2022-05-16 15:45:15 -0600

    Add type Path

--- server/io/file.go
+++ server/io/file.go
@@ -4,10 +4,32 @@
 
 // Package io Models a file system according to
 // https://github.com/tobiasbriones/cp-unah-mm545-distributed-text-file-system/tree/main/model
+// Author Tobias Briones
 package io
+
+import (
+	"errors"
+	"regexp"
+)
 
 const (
 	Root           = ""
 	Separator      = "/"
 	ValidPathRegex = "^$|\\\\w+/*\\\\.*-*"
 )
+
+type Path struct {
+	value string
+}
+
+func NewPath(value string) (Path, error) {
+	if !isValidPath(value) {
+		return Path{}, errors.New("invalid path")
+	}
+	return Path{value: value}, nil
+}
+
+func isValidPath(value string) bool {
+	match, _ := regexp.MatchString(ValidPathRegex, value)
+	return match
+}
