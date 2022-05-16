7fb8e6cd5fb57bb81a1411d03ec0bf8c11d1d500
Tobias Briones <tobiasbriones.dev@gmail.com>
2022-05-16 16:14:32 -0600

    Add test case TestNewPath

--- server/io/file_test.go
+++ server/io/file_test.go
@@ -3,3 +3,24 @@
 // This file is part of https://github.com/tobiasbriones/ep-file-system-server
 
 package io
+
+import "testing"
+
+func TestNewPath(t *testing.T) {
+	_, err := NewPath("")
+	requireNoError(t, err)
+
+	// Notice how everything is relative (no initial "/")
+	// Although the path "/" is also valid
+	_, err = NewPath("fs")
+	requireNoError(t, err)
+
+	_, err = NewPath("fs/file-1.txt")
+	requireNoError(t, err)
+}
+
+func requireNoError(t *testing.T, err error) {
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+}
