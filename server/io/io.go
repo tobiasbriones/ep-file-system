18f1925ba5e54c38a0ec2492b75a7b13f0553549
Tobias Briones <tobiasbriones.dev@gmail.com>
2022-05-16 22:27:38 -0600

    Add function getPath

--- server/io/io.go
+++ server/io/io.go
@@ -3,3 +3,25 @@
 // This file is part of https://github.com/tobiasbriones/ep-file-system-server
 
 package io
+
+import (
+	"strings"
+)
+
+const (
+	DefChannel = "main"
+	fsRootPath = "fs"
+)
+
+func getPath(relPath string, channel string) (Path, error) {
+	path, err := NewPathFrom(fsRootPath, channel)
+	if err != nil {
+		return Path{}, err
+	}
+	children := strings.Split(relPath, Separator)
+	err = path.Append(children...)
+	if err != nil {
+		return Path{}, err
+	}
+	return path, nil
+}
