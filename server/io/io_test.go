86df79e9173fef7430b46b4d7b1b1632b08adeb4
Tobias Briones <tobiasbriones.dev@gmail.com>
2022-05-16 22:28:04 -0600

    Add test case TestGetPath

--- server/io/io_test.go
+++ server/io/io_test.go
@@ -3,3 +3,19 @@
 // This file is part of https://github.com/tobiasbriones/ep-file-system-server
 
 package io
+
+import "testing"
+
+func TestGetPath(t *testing.T) {
+	path, err := getPath("file.txt", DefChannel)
+	requireNoError(t, err)
+	if path.value != "fs/main/file.txt" {
+		t.Fatal("Computed path is wrong")
+	}
+
+	path, err = getPath("dir1/dir2/file.txt", DefChannel)
+	requireNoError(t, err)
+	if path.value != "fs/main/dir1/dir2/file.txt" {
+		t.Fatal("Computed path is wrong")
+	}
+}
