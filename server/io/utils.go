b038ec250e5b7dced2fb47818f6e9bc9c27c5a36
Tobias Briones <tobiasbriones.dev@gmail.com>
2022-05-16 18:26:41 -0600

    Add IO utility functions

--- server/io/utils.go
+++ server/io/utils.go
@@ -7,3 +7,34 @@ package io
 // Package io(utils) implements basic OS file operations related to this IO
 // module.
 // Author Tobias Briones
+
+import "os"
+
+func createFile(path string) error {
+	f, err := os.Create(path)
+	f.Close()
+	return err
+}
+
+func writeBuf(path string, buf []byte) error {
+	f, err := os.OpenFile(path, os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil {
+		return err
+	}
+	_, err = f.Write(buf)
+	f.Close()
+	return err
+}
+
+func readFileSize(path string) (int64, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return 0, err
+	}
+	defer f.Close()
+	fi, err := f.Stat()
+	if err != nil {
+		return 0, err
+	}
+	return fi.Size(), nil
+}
