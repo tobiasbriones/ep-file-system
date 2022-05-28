a543e6bea3c12776be58ac9c7c97ba0efc827883
Tobias Briones <tobiasbriones.dev@gmail.com>
2022-05-27 22:01:26 -0600

    Add struct User

--- process/user.go
+++ process/user.go
@@ -3,3 +3,14 @@
 // This file is part of https://github.com/tobiasbriones/ep-tcp-file-system
 
 package process
+
+import "fs"
+
+// User Contains all the FSM implementation details.
+type User struct {
+	file     fs.OsFile
+	channel  Channel
+	osFsRoot string
+	size     uint64
+	count    uint64
+}
