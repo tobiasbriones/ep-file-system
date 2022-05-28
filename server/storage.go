e0392b190ec15f18330fff7cd5e2832e03dc0836
Tobias Briones <tobiasbriones.dev@gmail.com>
2022-05-27 23:33:33 -0600

    Add storage.go on package main
    This module defines the physical location of the server file system.

---
+++ server/storage.go
@@ -0,0 +1,9 @@
+// Copyright (c) 2022 Tobias Briones. All rights reserved.
+// SPDX-License-Identifier: BSD-3-Clause
+// This file is part of https://github.com/tobiasbriones/ep-tcp-file-system
+
+package main
+
+const (
+	fsRoot = ".fs"
+)
