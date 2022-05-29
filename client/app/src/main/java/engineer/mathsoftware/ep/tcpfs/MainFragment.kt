123d72104bc9b872a918f0d956ed9bbf922c2415
Tobias Briones <tobiasbriones.dev@gmail.com>
2022-05-29 16:52:16 -0600

    Refactor rename fragment from FirstFragment to MainFragment

--- client/app/src/main/java/engineer/mathsoftware/ep/tcpfs/MainFragment.kt
+++ client/app/src/main/java/engineer/mathsoftware/ep/tcpfs/MainFragment.kt
@@ -1,19 +1,18 @@
 package engineer.mathsoftware.ep.tcpfs
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import engineer.mathsoftware.ep.tcpfs.databinding.FragmentFirstBinding
+import engineer.mathsoftware.ep.tcpfs.databinding.FragmentMainBinding
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
-
-    private var _binding: FragmentFirstBinding? = null
+class MainFragment : Fragment() {
+    private var _binding: FragmentMainBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -24,7 +23,7 @@ class FirstFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
         return binding.root
     }
 
