30bacf6a9bb428c62effb4d4c00c6b8b230beb79
Tobias Briones <tobiasbriones.dev@gmail.com>
2022-05-29 21:40:08 -0600

    Add process values

--- client/app/src/main/java/engineer/mathsoftware/ep/tcpfs/Client.kt
+++ client/app/src/main/java/engineer/mathsoftware/ep/tcpfs/Client.kt
@@ -1,7 +1,22 @@
 package engineer.mathsoftware.ep.tcpfs
 
-const val HOST = "localhost"
-const val PORT = 8080
+enum class Action {
+    UPLOAD,
+    DOWNLOAD
+}
+
+enum class State {
+    START,
+    DATA,
+    STREAM,
+    EOF,
+    DONE,
+    ERROR
+}
+
+const val HOST: String = "localhost"
+const val PORT: Int = 8080
+const val URI: String = "http://$HOST:$PORT"
 
 class Client {
 
