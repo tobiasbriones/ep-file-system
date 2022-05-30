e9c782ca91a2d71ad3f3bc3b1766821da6f17bb1
Tobias Briones <tobiasbriones.dev@gmail.com>
2022-05-29 21:41:09 -0600

    Add basic socket methods

--- client/app/src/main/java/engineer/mathsoftware/ep/tcpfs/Client.kt
+++ client/app/src/main/java/engineer/mathsoftware/ep/tcpfs/Client.kt
@@ -1,5 +1,12 @@
 package engineer.mathsoftware.ep.tcpfs
 
+import android.util.Log
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import java.net.ConnectException
+import java.net.InetAddress
+import java.net.Socket
+
 enum class Action {
     UPLOAD,
     DOWNLOAD
@@ -14,10 +21,28 @@ enum class State {
     ERROR
 }
 
-const val HOST: String = "localhost"
+const val HOST: String = "10.0.2.2" // This localhost IP works on the emulator
 const val PORT: Int = 8080
 const val URI: String = "http://$HOST:$PORT"
 
 class Client {
+    var file: String = ""
+    private var channel: String = "test"
+    private lateinit var socket: Socket
 
+    suspend fun connect() {
+        withContext(Dispatchers.IO) {
+            val address = InetAddress.getByName(HOST)
+            try {
+                socket = Socket(address, PORT)
+                Log.d("UPLOAD", "Connection established")
+            }catch (e: ConnectException) {
+                Log.d("UPLOAD", e.message.toString())
+            }
+        }
+    }
+
+    fun disconnect() {
+        socket.close()
+    }
 }
