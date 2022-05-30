b391b7669fa5d844be7f22d1edd6e7806a07fac8
Tobias Briones <tobiasbriones.dev@gmail.com>
2022-05-29 18:02:22 -0600

    Implement file picker with ByteArray to load the file data

--- client/app/src/main/java/engineer/mathsoftware/ep/tcpfs/Files.kt
+++ client/app/src/main/java/engineer/mathsoftware/ep/tcpfs/Files.kt
@@ -1,2 +1,27 @@
 package engineer.mathsoftware.ep.tcpfs
 
+import android.content.ContentResolver
+import android.net.Uri
+import java.io.ByteArrayOutputStream
+import java.io.InputStream
+
+const val SERVER_BUF_SIZE = 1024
+
+fun read(res: ContentResolver, uri: Uri): ByteArray {
+    val stream: InputStream? = res.openInputStream(uri)
+    if (stream != null) {
+        return getBytes(stream)
+    }
+    return ByteArray(0)
+}
+
+private fun getBytes(inputStream: InputStream): ByteArray {
+    val byteBuffer = ByteArrayOutputStream()
+    val buffer = ByteArray(SERVER_BUF_SIZE)
+    var len = 0
+    while (inputStream.read(buffer)
+            .also { len = it } != -1) {
+        byteBuffer.write(buffer, 0, len)
+    }
+    return byteBuffer.toByteArray()
+}
--- client/app/src/main/java/engineer/mathsoftware/ep/tcpfs/MainFragment.kt
+++ client/app/src/main/java/engineer/mathsoftware/ep/tcpfs/MainFragment.kt
@@ -1,11 +1,17 @@
 package engineer.mathsoftware.ep.tcpfs
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import engineer.mathsoftware.ep.tcpfs.databinding.FragmentMainBinding
+
+const val PICKFILE_REQUEST_CODE = 1
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -39,7 +45,36 @@ class MainFragment : Fragment() {
         _binding = null
     }
 
+    override fun onActivityResult(
+        requestCode: Int,
+        resultCode: Int,
+        data: Intent?
+    ) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode != Activity.RESULT_OK) {
+            return
+        }
+        if (data == null) {
+            return
+        }
+        when (requestCode) {
+            PICKFILE_REQUEST_CODE -> readFileToUpload(data.data)
+        }
+    }
+
     private fun chooseFileToUpload() {
-        // TODO
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
+            addCategory(Intent.CATEGORY_OPENABLE)
+            type = "*/*"
+        }
+        startActivityForResult(intent, PICKFILE_REQUEST_CODE)
+    }
+
+    private fun readFileToUpload(data: Uri?) {
+        var bytes = ByteArray(0)
+        if (data != null) {
+            context?.let { bytes = read(it.contentResolver, data) }
+        }
+        Log.d("UPLOAD", bytes.size.toString())
     }
 }
