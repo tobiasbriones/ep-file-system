02c3f44efe5472c043bda51aa5c3c15383cfa8c2
Tobias Briones <tobiasbriones.dev@gmail.com>
2022-05-30 20:02:43 -0600

    Add function StringSliceContains to package utils

--- utils/utils.go
+++ utils/utils.go
@@ -23,3 +23,14 @@ func RequireNoError(e error) {
 		panic(e)
 	}
 }
+
+// StringSliceContains https://play.golang.org/p/Qg_uv_inCek
+// checks if a string is present in a slice
+func StringSliceContains(s []string, str string) bool {
+	for _, v := range s {
+		if v == str {
+			return true
+		}
+	}
+	return false
+}
