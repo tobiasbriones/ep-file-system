d8120f81681d36139b66fcca7c4d693863620833
Tobias Briones <tobiasbriones.dev@gmail.com>
2022-05-31 12:38:42 -0600

    Refactor rename fragment from SecondFragment to ClientFragment

--- client/app/src/main/java/engineer/mathsoftware/ep/tcpfs/ClientFragment.kt
+++ client/app/src/main/java/engineer/mathsoftware/ep/tcpfs/ClientFragment.kt
@@ -10,13 +10,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
-import engineer.mathsoftware.ep.tcpfs.databinding.FragmentSecondBinding
+import engineer.mathsoftware.ep.tcpfs.databinding.FragmentClientBinding
 
-/**
- * A simple [Fragment] subclass as the second destination in the navigation.
- */
-class SecondFragment : Fragment() {
-    private var _binding: FragmentSecondBinding? = null
+class ClientFragment : Fragment() {
+    private var _binding: FragmentClientBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -26,8 +23,7 @@ class SecondFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
-        _binding = FragmentSecondBinding.inflate(inflater, container, false)
+        _binding = FragmentClientBinding.inflate(inflater, container, false)
         return binding.root
     }
 
