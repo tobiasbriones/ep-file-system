69ac1cde71e5390a2f3cc550b46b1c26b0d3231e
Tobias Briones <tobiasbriones.dev@gmail.com>
2022-05-30 21:22:15 -0600

    Add test case TestFileList

--- server/main_test.go
+++ server/main_test.go
@@ -219,6 +219,36 @@ func TestChannelList(t *testing.T) {
 	}
 }
 
+func TestFileList(t *testing.T) {
+	tcpAddr, err := net.ResolveTCPAddr(network, getServerAddress())
+	utils.RequirePassCase(t, err, "Fail to resolve TCP address")
+	conn, err := net.DialTCP(network, nil, tcpAddr)
+	defer conn.Close()
+	utils.RequirePassCase(t, err, "Fail to establish connection")
+
+	// Send command
+	cmd := make(map[string]string)
+	cmd["REQ"] = "LIST_FILES"
+	cmd["CHANNEL"] = testChannel
+	msg := Message{
+		Command: cmd,
+	}
+	b, err := json.Marshal(msg)
+	_, err = conn.Write(b)
+	utils.RequirePassCase(t, err, "Fail to write command to the server")
+
+	// Receive response
+	var fileList []string
+	dec := json.NewDecoder(conn)
+	err = dec.Decode(&fileList)
+	if err != nil {
+		return
+	}
+
+	// Check
+	log.Println("Files on channel test:", fileList)
+}
+
 // Tests if the server closes the connection after the read timeout is consumed.
 func TestTcpTimeout(t *testing.T) {
 	if testing.Short() {
