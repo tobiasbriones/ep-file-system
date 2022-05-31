c291afe6041cd17d110938b504ba601656fcd474
Tobias Briones <tobiasbriones.dev@gmail.com>
2022-05-30 18:21:00 -0600

    Add client copyright license header

--- client/app/src/main/java/engineer/mathsoftware/ep/tcpfs/Conn.kt
+++ client/app/src/main/java/engineer/mathsoftware/ep/tcpfs/Conn.kt
@@ -1,1 +1,5 @@
+// Copyright (c) 2022 Tobias Briones. All rights reserved.
+// SPDX-License-Identifier: BSD-3-Clause
+// This file is part of https://github.com/tobiasbriones/ep-tcp-file-system
+
 package engineer.mathsoftware.ep.tcpfs
--- client/app/src/main/java/engineer/mathsoftware/ep/tcpfs/Files.kt
+++ client/app/src/main/java/engineer/mathsoftware/ep/tcpfs/Files.kt
@@ -1,3 +1,7 @@
+// Copyright (c) 2022 Tobias Briones. All rights reserved.
+// SPDX-License-Identifier: BSD-3-Clause
+// This file is part of https://github.com/tobiasbriones/ep-tcp-file-system
+
 package engineer.mathsoftware.ep.tcpfs
 
 import android.content.ContentResolver
