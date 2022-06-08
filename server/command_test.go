b2466c34dadbfc5023c241eb412cb3a52886108a
Tobias Briones <tobiasbriones.dev@gmail.com>
2022-06-07 21:32:02 -0600

    Move other tests for commands

--- server/command_test.go
+++ server/command_test.go
@@ -78,3 +78,67 @@ func TestCommandListOfClients(t *testing.T) {
 	// Check
 	log.Println("Connected users:", users)
 }
+
+func TestCommandChannelList(t *testing.T) {
+	tcpAddr, err := net.ResolveTCPAddr(network, getServerAddress())
+	utils.RequirePassCase(t, err, "Fail to resolve TCP address")
+	conn, err := net.DialTCP(network, nil, tcpAddr)
+	defer conn.Close()
+	utils.RequirePassCase(t, err, "Fail to establish connection")
+
+	// Send command
+	cmd := make(map[string]string)
+	cmd["REQ"] = "LIST_CHANNELS"
+	msg := Message{
+		Command: cmd,
+	}
+	b, err := json.Marshal(msg)
+	_, err = conn.Write(b)
+	utils.RequirePassCase(t, err, "Fail to write command to the server")
+
+	// Receive response
+	var channels []string
+	dec := json.NewDecoder(conn)
+	err = dec.Decode(&channels)
+	if err != nil {
+		return
+	}
+
+	// Check at least has the main, and test channels
+	if !utils.StringSliceContains(channels, "main") {
+		t.Fatal("Channel list does not contain channel: main")
+	}
+	if !utils.StringSliceContains(channels, "test") {
+		t.Fatal("Channel list does not contain channel: test")
+	}
+}
+
+func TestCommandFileList(t *testing.T) {
+	tcpAddr, err := net.ResolveTCPAddr(network, getServerAddress())
+	utils.RequirePassCase(t, err, "Fail to resolve TCP address")
+	conn, err := net.DialTCP(network, nil, tcpAddr)
+	defer conn.Close()
+	utils.RequirePassCase(t, err, "Fail to establish connection")
+
+	// Send command
+	cmd := make(map[string]string)
+	cmd["REQ"] = "LIST_FILES"
+	cmd["CHANNEL"] = testChannel
+	msg := Message{
+		Command: cmd,
+	}
+	b, err := json.Marshal(msg)
+	_, err = conn.Write(b)
+	utils.RequirePassCase(t, err, "Fail to write command to the server")
+
+	// Receive response
+	var fileList []string
+	dec := json.NewDecoder(conn)
+	err = dec.Decode(&fileList)
+	if err != nil {
+		return
+	}
+
+	// Check
+	log.Println("Files on channel test:", fileList)
+}
