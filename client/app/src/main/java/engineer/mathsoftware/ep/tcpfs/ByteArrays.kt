186019f12b56028c612d5ab773cda7a7fe9d054b
Tobias Briones <tobiasbriones.dev@gmail.com>
2022-06-08 13:07:22 -0600

    Create ByteArrays.kt

---
+++ client/app/src/main/java/engineer/mathsoftware/ep/tcpfs/ByteArrays.kt
@@ -0,0 +1,6 @@
+// Copyright (c) 2022 Tobias Briones. All rights reserved.
+// SPDX-License-Identifier: BSD-3-Clause
+// This file is part of https://github.com/tobiasbriones/ep-tcp-file-system
+
+package engineer.mathsoftware.ep.tcpfs
+
