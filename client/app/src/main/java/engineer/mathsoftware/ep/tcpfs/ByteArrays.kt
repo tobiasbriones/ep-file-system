8aa8f1a5249a75f444049419ff1f187af93e1da4
Tobias Briones <tobiasbriones.dev@gmail.com>
2022-06-08 13:17:36 -0600

    Add attribute type

--- client/app/src/main/java/engineer/mathsoftware/ep/tcpfs/ByteArrays.kt
+++ client/app/src/main/java/engineer/mathsoftware/ep/tcpfs/ByteArrays.kt
@@ -27,3 +27,18 @@ fun ByteArray.parseArray(): JSONArray {
     return JSONArray(String(this))
 }
 
+// This is a low-level response parsing, a high-level construct like states
+// must be used to understand the actual meaning of the server.
+//
+// For example: It can be a response that is a data chunk for a small JSON
+// text file, but this can be interpreted as a server message or as a data
+// chunk, it depends on the machine state.
+fun ByteArray.type(): DataType {
+    val str = String(this)
+    if (str.isEmpty()) return RAW
+    return when (str[0]) {
+        '{'  -> MESSAGE
+        '['  -> ARRAY
+        else -> RAW
+    }
+}
